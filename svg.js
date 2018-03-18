ca945f76ec8946a85de3f5b2daefaf13444c50c9
zme1 <zme1@pitt.edu>
2018-03-18 13:29:58 -0400

    Revert "adding most updated versions of my svg and related files" reverting recent commit This reverts commit a74dd0e7265b09f01a19718481267e1ec2bc92db.

--- svg.js
+++ svg.js
@@ -1,12 +1,1 @@
-window.addEventListener('DOMContentLoaded', init, false);
-
-function init(){
-    var acceptedBars = document.getElementsByClassName('toggle');
-    for (var i = 0; i < acceptedBars.length; i++) {
-        acceptedBars[i].addEventListener('click', toggleTable, false);
-    }
-}
-
-function toggleTable() {
-alert('Hi!');
-}
+window.addEventListener('DOMContentLoaded', alert('hi'), false);
